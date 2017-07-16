9424c87cedc4a37478d7cf4d9af40047e9f9df87
Philipp Schreitmueller <philipp.schreitmueller@in.tum.de>
2017-07-16 22:10:09 +0200

    plugin description

---
+++ wp-content/plugins/instainteressant/insta-interessant.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Insta-Interessant
+Plugin URI:  https://ph91.de
+Description: Basic WordPress Plugin Header Comment
+Version:     v0.1
+Author:      Philipp Schreitmueller
+
+*/
+
+use MetzWeb\Instagram
+
+)
